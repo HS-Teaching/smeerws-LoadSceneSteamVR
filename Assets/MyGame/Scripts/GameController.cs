73790d287da6acf44c7cb5d89e36e7b0bbc49b1e
smeerws <hello@smeerws.at>
2020-02-02 13:58:53 +0100

    add empty gameobjects as script holder

---
+++ Assets/MyGame/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject triggerCube;
+    [SerializeField]
+    private GameObject placeHolderSceneSwitch;
+
+    private Interactable interactable;
+    private bool switchScene = false;
+
+    private void Awake ()
+    {
+        interactable = triggerCube.GetComponent<Interactable>();
+    }
+
+    private void Update ()
+    {
+        if (!switchScene && interactable.isHovering)
+        {
+            switchScene = true;
+        }
+
+        if (switchScene)
+        {
+            placeHolderSceneSwitch.SetActive (true);
+        }
+    }
+}
